7b4ad1a9d8ed0106846f87b230168a670f63a367
sh3zb0t <49079179+sh3zb0t@users.noreply.github.com>
2025-02-26 11:15:29 +0100

    Create main.c
    programanie od 1 do 5

---
+++ main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //printowanie
+    /*
+    int a=2137;
+    float b=21.37;
+    char c[]= "siema";
+
+    printf("%d" ,a);
+    printf("\n%f" , b);
+    printf( "\n%s" , c );
+    printf( "\nkoniec");
+    return 0;
+    */
+    //dodawanie + dzielenie
+    /*float a = 2.1;
+    float b = 3.7;
+    float c = 6.9;
+    float d = (a+b+c)/3;
+    printf("wynik: %f",d);
+    */
+    //mnożenie
+    /*
+    int a = 4;
+    int b = 2;
+    int c = a*b;
+    printf("%d",c);*/
+    //celcjusze na farenheity
+    /*
+    float C = 28;
+    float F = C * 9/5 + 32;
+    printf( "%.2f", C );
+    printf(" stopni celcjusza to %.2f", F);
+    printf(" farenheita");
+    */
+    //numerki indeksu
+    /*int a = 5;
+    int b = 3;
+    int c = a/b;
+    printf("dzielenie w intach: %d", c);
+    printf("\ndzielenie w floacie: %f", a/(float)b);
+    printf("\nwniosek: int nie przechowuje przecinków");*/
+    //bulbulator
+    float a = 21.37;
+    float b = 13.37;
+    printf("dodawanie: %f\nmnożenie: %f\ndzielenie: %f\nodejmowanie: %f", a+b, a*b, a/b, a-b );
+}
